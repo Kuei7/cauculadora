7d21e63e9513846410986452c7e883fed4219479
unknown <gustavoghkuei@gmail.com>
2024-06-07 21:06:43 -0300

    segundo commit

---
+++ Projeto 1/aula2.2.py
@@ -0,0 +1,4 @@
+peso_corporal = float(input("Escreva o seu peso"))
+altura = float(input("Escreva a sua altura"))
+resultado_altura = (altura ** 2)
+imc = (peso_corporal / resultado_altura)
---
+++ Projeto 1/aula2.py
@@ -0,0 +1,15 @@
+primeiro_numero = float(input("Escreva o primeiro Numero"))
+Operador = input("Operador")
+segundo_numero = float(input("Escreva o segundo numero"))
+if (Operador=="+"):
+    resultado = (primeiro_numero + segundo_numero)
+    print(resultado)
+if (Operador=="-"):
+    resultado = (primeiro_numero + segundo_numero)
+    print(resultado)
+if (Operador=="*"):
+    resultado = (primeiro_numero + segundo_numero)
+    print(resultado)
+if (Operador=="/"):
+    resultado = (primeiro_numero + segundo_numero)
+    print(resultado)
